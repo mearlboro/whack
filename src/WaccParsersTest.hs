20e41b028594f5cfa475c884e6f9bb32b054ea03
lmc13 <lorenzo.calegari13@imperial.ac.uk>
2014-10-31 23:12:15 +0000

    Improved test suite

--- src/WaccParsersTest.hs
+++ src/WaccParsersTest.hs
@@ -6,54 +6,84 @@ import Text.ParserCombinators.Parsec
 
 
 tPairLiter 
-  = [ ( "pPairLiter" , pPairLiter , "null"  , True  )
-    , ( "pPairLiter" , pPairLiter , ""      , False )
-    , ( "pPairLiter" , pPairLiter , " null" , False )
-    , ( "pPairLiter" , pPairLiter , " Null" , False )
-    , ( "pPairLiter" , pPairLiter , " null" , False )
-    , ( "pPairLiter" , pPairLiter , "Null " , False ) ]
+  = [ ( "null"  , True  )
+    , ( ""      , False )
+    , ( " null" , False )
+    , ( " Null" , False )
+    , ( " null" , False )
+    , ( "Null " , False ) ]
 
 tArrayLiter 
-  = [ ( "pArrayLiter" , pArrayLiter , "" ,   True  )
-    , ( "pArrayLiter" , pArrayLiter , " " ,  True  )
-    , ( "pArrayLiter" , pArrayLiter , "(" ,  True  ) ]
+  = [ ( ""    , False )
+    , ( "["   , False )
+    , ( "]"   , False )
+    , ( "[]"  , True  )
+    , ( "[,]" , False ) ]
 
 tIntSign 
-  = [ ( "pIntSign" , pIntSign , "+" ,   True  ) 
-    , ( "pIntSign" , pIntSign , "-" ,   True  ) 
-    , ( "pIntSign" , pIntSign , "a" ,   True  ) 
-    , ( "pIntSign" , pIntSign , "a" ,   True  ) 
-    , ( "pIntSign" , pIntSign , "" ,    True  ) ]
+  = [ ( "+" , True ) 
+    , ( "-" , True ) 
+    , ( "a" , True ) 
+    , ( "a" , True ) 
+    , ( ""  , True ) ]
 
 tIntLiter
-  = [ ( "pIntLiter" , pIntLiter , "12345" , True ) 
-    , ( "pIntLiter" , pIntLiter , "+12345" , True )
-    , ( "pIntLiter" , pIntLiter , "-12345" , True )
-    , ( "pIntLiter" , pIntLiter , "0" , True )
-    , ( "pIntLiter" , pIntLiter , "+0" , True )
-    , ( "pIntLiter" , pIntLiter , "-0" , True )
-    , ( "pIntLiter" , pIntLiter , "000000" , True )
-    , ( "pIntLiter" , pIntLiter , "-000111" , True )
-    , ( "pIntLiter" , pIntLiter , "+111100" , True )
-    , ( "pIntLiter" , pIntLiter , "" , False )
-    , ( "pIntLiter" , pIntLiter , "+" , False )
-    , ( "pIntLiter" , pIntLiter , "-" , False )
-    , ( "pIntLiter" , pIntLiter , "a" , False )
-    , ( "pIntLiter" , pIntLiter , "+a" , False )
-    , ( "pIntLiter" , pIntLiter , "-a" , False )
-    , ( "pIntLiter" , pIntLiter , "hello" , False )
-    , ( "pIntLiter" , pIntLiter , "+1x" , True ) ]
+  = [ ( "12345"   , True  ) 
+    , ( "+12345"  , True  )
+    , ( "-12345"  , True  )
+    , ( "0"       , True  )
+    , ( "+0"      , True  )
+    , ( "-0"      , True  )
+    , ( "000000"  , True  )
+    , ( "-000111" , True  )
+    , ( "+111100" , True  )
+    , ( "+1x"     , True  )
+    , ( ""        , False )
+    , ( "+"       , False )
+    , ( "-"       , False )
+    , ( "a"       , False )
+    , ( "+a"      , False )
+    , ( "-a"      , False )
+    , ( "hello"   , False ) ]
 
+tComment 
+  = [ ( ""                     , False ) 
+    , ( "1"                    , False )
+    , ( "a"                    , False )
+    , ( "\n"                   , False )
+    , ( "\t"                   , False )
+    , ( "\""                   , False )
+    , ( "#"                    , False )
+    , ( "#hello"               , False )
+    , ( "#"                    , False )
+    , ( "#\n"                  , True  )
+    , ( "#1\n"                 , True  )
+    , ( "#a\n"                 , True  )
+    , ( "#\"\'\t\b\f\0Allah\n" , True  )
+    , ( "# Hello World\n"      , True  )
+    , ( "########\n"           , True  )
+    , ( "# \n"                 , True  )
+    , ( "# ##\na"              , True  )
+    , ( "# :@òasd%313412&&&\n" , True  ) ]
+
+
+-- To create a test for a parsing function `pFuncName`
+-- 1) Create test array `tFuncName` :: ( str::String , pass::Bool )
+--    With input string to parse `str` and whether or not it should `pass`
+-- 2) ghci> runTests "FuncName" pFuncName tFuncName
+-- 3) add the line above to `runAll`
 
 runAll = do
-  runTests tPairLiter
-  runTests tArrayLiter
-  runTests tIntSign
-  runTests tIntLiter
-  runTests tArrayElem
+  runTests "pPairLiter"  pPairLiter  tPairLiter
+  runTests "pArrayLiter" pArrayLiter tArrayLiter
+  runTests "pIntSign"    pIntSign    tIntSign
+  runTests "pIntLiter"   pIntLiter   tIntLiter
+  runTests "pComment"    pComment    tComment
 
 
-runTest ( fname , parser , input , pass ) = case regularParse parser input of 
+runTests fname parser tests = mapM_ ( runTest fname parser ) tests
+
+runTest fname parser ( input , pass ) = case regularParse parser input of 
   Left  e -> log $ if not pass then "OK it failed!"
                    else "='( Failed but should've passed... ("  ++ show e ++ ")"
   Right r -> log $ if pass then "OK it succeeded! (" ++ show r ++ ")" 
@@ -63,7 +93,6 @@ runTest ( fname , parser , input , pass ) = case regularParse parser input of
 
 
 
-runTests tests = mapM_ runTest tests
    
 
 
