22e8a35f584bb4b5558e042cf4c2e36ba97de233
lmc13 <lorenzo.calegari13@imperial.ac.uk>
2014-10-31 14:11:24 +0000

    Added language definition, lexer, operators defintions and initial parsing functions

--- WaccDataTypes.hs
+++ WaccDataTypes.hs
@@ -1,3 +1,7 @@
+module WaccDataTypes 
+((..))
+
+where
 
 import System.IO
 import Control.Monad
@@ -235,6 +239,234 @@ data PairLiter                                  -- <pair-liter> ::=
   
 type Comment = [ Char ]                         -- <comment> ::= '#' (any-character-except-EOL)* (EOL)
 
-      
+ 
+
+reservedWords :: [ String ]
+reservedWords 
+  = [ "begin" , "end", "is", "skip", "read", "free", "return", "exit", "print", "println"
+    , "if", "then", "else", "fi", "while", "do", "done", "call", "newpair", "fst", "snd"
+    , "int", "bool", "char", "string", "pair", "len", "ord", "chr", "null", "true", "false" ]
+
+reservedOperators :: [ String ]
+reservedOperators
+  = [ "+",  "-", "*",  "/",  "=",  "==", "!=", "<", ">", "<="
+    , ">=", "&&", "||", "%", "len", "ord", "chr", "!"] 
+
+languageDef -- TODO write type signature
+  = emptyDef 
+  { Token.commentLine     = "#"
+  , Token.identStart      = letter   <|> char '_'
+  , Token.identLetter     = alphaNum <|> char '_'
+  , Token.reservedNames   = reservedWords
+  , Token.reservedOpNames = reservedOperators 
+  }
+
+lexer = Token.makeTokenParser languageDef -- TODO write type signature
+
+identifier = Token.identifier lexer -- parses an identifier -- TODO write type signature
+reserved   = Token.reserved   lexer -- parses a reserved name
+reservedOp = Token.reservedOp lexer -- parses an operator
+parens     = Token.parens     lexer -- parses surrounding parenthesis: parens p takes care of the parenthesis and uses p to parse what's inside them
+integer    = Token.integer    lexer -- parses an integer
+semi       = Token.semi       lexer -- parses a semicolon
+whiteSpace = Token.whiteSpace lexer -- parses whitespace
+     
+-- :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: --
+-- :: Abstract Data Tree Formation :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: --
+-- :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: --
+
+-- :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: --
+-- :: <program> PARSER 
+
+
+-- :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: --
+-- :: <func> PARSER 
+
+-- :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: --
+-- :: <func> PARSER 
+
+
+
+-- :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: --
+-- :: Expression PARSER 
+
+-- :: Arithetic expression :: --
+arithExpr :: Parser Expr
+arithExpr = buildExpressionParser arithOpers arithTerm
+ 
+-- :: Boolean expression :: --
+boolExpr :: Parser Expr
+boolExpr = buildExpressionParser boolOpers boolTerm
+
+-- :: Arithmetic operators :: --
+arithOpers 
+  = [ [ Prefix ( reservedOp "!"   >> return NotUnOp        )           ]
+    , [ Prefix ( reservedOp "len" >> return LenUpOp        )           ]
+    , [ Prefix ( reservedOp "ord" >> return OrdUpOp        )           ]
+    , [ Prefix ( reservedOp "chr" >> return ChrUnOp        )           ]
+    , [ Prefix ( reservedOp "-"   >> return NegUnOp        ) AssocLeft ] 
+    , [ Infix  ( reservedOp "+"   >> return PlusBinOp      ) AssocLeft ] 
+    , [ Infix  ( reservedOp "*"   >> return TimesBinOp     ) AssocLeft ] 
+    , [ Infix  ( reservedOp "/"   >> return DivBinOp       ) AssocLeft ] 
+    , [ Infix  ( reservedOp "%"   >> return ModBinOp       ) AssocLeft ] 
+    , [ Infix  ( reservedOp "<"   >> return LessBinOp      ) AssocLeft ]
+    , [ Infix  ( reservedOp "<="  >> return LessEqBinOp    ) AssocLeft ] 
+    , [ Infix  ( reservedOp ">"   >> return GreaterBinOp   ) AssocLeft ] 
+    , [ Infix  ( reservedOp ">="  >> return GreaterEqBinOp ) AssocLeft ] ]
+
+-- :: Bolean operators :: --
+bOperators 
+  = [ [ Infix  ( reservedOp "=="  >> return EqualsBinOp    ) AssocLeft ]
+    , [ Infix  ( reservedOp "!="  >> return NotEqualsBinOp ) AssocLeft ]
+    , [ Infix  ( reservedOp "&&"  >> return AndBinOp       ) AssocLeft ]
+    , [ Infix  ( reservedOp "||"  >> return OrBinOp        ) AssocLeft ] ]
+  
+-- :: Arithmetic term :: --
+arithTerm :: Parser Expr
+arithTerm = parens arithExpr
+         <|> 
+
+-- :: Boolean term :: --
+boolTerm = undefined
+
+
+
+
+pExpr :: Parser Expr
+pExpr = fail "Implement!"
+
+
+waccParser :: Parser Program
+waccParser = whiteSpace >> pProgram
+
+
+pProgram :: Parser Program -- 'begin' <func>* <stat> 'end'
+pProgram = do 
+  reserved "begin"
+  funcs <- many pFunc
+  stat  <- pStat
+  reserved "end"
+  return $ Program funcs stat
+
+
+pFunc :: Parser Func -- <type> <ident> '(' <param-list>? ')' 'is' <stat> 'end' 
+pFunc = do 
+  typez <- pType   
+  ident <- identifier
+  pList <- parens pParamList
+  reserved "is"
+  stat  <- pStatement
+  reserved "end"
+  return $ Func typez ident pList stat
+
+
+pType :: Parser Type
+pType = fail "Implement!"
+
+
+pParamList :: Parser ParamList
+pParamList = fail "Implement!"
+
+
+pStat :: Parser Stat
+pStat = fail "Implement!"
+
+
+pStatement :: Parser Stat
+pStatement = fail "Implement!"
+
+
+
+
+-- To parse values of type `a` we need a `Parser a`. 
+charParser :: Parser Char
+-- Parsec comes with `anyChar` that parses any character...
+charParser = anyChar
+
+
+regularParse :: Parser a -> String -> Either ParseError a
+regularParse p = parse p ""
+
+
+--This function will run the parser, but additionally fail if it doesn't
+--consume all the input.
+parseWithEof :: Parser a -> String -> Either ParseError a
+parseWithEof p = parse ( p <* eof ) ""
+
+--This function will apply the parser, then also return any left over
+--input which wasn't parsed.
+parseWithLeftOver :: Parser a -> String -> Either ParseError (a,String)
+parseWithLeftOver p = parse ( (,) <$> p <*> leftOver ) ""
+  where leftOver = manyTill anyToken eof
+
+
+{-
+
+type Parser = Parsec String ()
+
+type Parsec s u = ParsecT s u Identity
+
+
+`ParsecT s u m a` is a parser with stream type `s`, user state type `u`,
+underlying monad `m` and return type `a`.
+
+
+-- The `char` parser parses a specific character which you supply:
+char     :: Char -> Parser Char
+
+upper    :: Parser Char
+lower    :: Parser Char
+alphaNum :: Parser Char
+letter   :: Parser Char
+digit    :: Parser Char
+hexDigit :: Parser Char
+octDigit :: Parser Char
+
+oneOf    :: [Char] -> Parser Char
+noneOf   :: [Char] -> Parser Char
+
+spaces :: Parser ()
+
+space   :: Parser Char
+newline :: Parser Char
+tab     :: Parser Char
+
+
+satisfy :: (Char -> Bool) -> Parser Char
+
+
+many1 :: Parser a -> Parser [a]
+It applies the parser given one or more times, returning the result.
+
+many parses zero or more items rather than one or more
+-}
+
+-- Parsec has `regularParse`, which take a Parser, a String to parse and
+-- either returns what it parsed or an error if it didn't match the stuff to parse
+--myParser :: Parser a -> String -> Either ParseError a
+--myParser = regularParse
+
+-- To parse an integer we need to parse the digits and the read the result as an int
+
+integerParser :: Parser Integer
+integerParser = do 
+    n <- many1 digit
+    return ( read n )
+
+integerParser' :: Parser Integer
+integerParser' = many1 digit >>= \n -> return $ read n 
+
+
+variableNameParser :: Parser String
+variableNameParser = do
+    first <- firstCharParser
+    rest  <- many restOfNameParser
+    return ( first:rest )  
+  where
+    firstCharParser :: Parser Char
+    firstCharParser = satisfy ( \c -> c == '_' || isLetter c )
+    restOfNameParser :: Parser Char 
+    restOfNameParser = satisfy ( \c -> isDigit c || isLetter c || c == '_' )
+   
 
 
