be714e7e0e5766149383dadb8ee63a57128869b4
lmc13 <lorenzo.calegari13@imperial.ac.uk>
2014-10-31 21:13:34 +0000

    Added more tests

--- src/WaccParsersTest.hs
+++ src/WaccParsersTest.hs
@@ -5,7 +5,7 @@ import WaccParser
 import Text.ParserCombinators.Parsec
 
 
-testsPairLiter 
+tPairLiter 
   = [ ( "pPairLiter" , pPairLiter , "null"  , True  )
     , ( "pPairLiter" , pPairLiter , ""      , False )
     , ( "pPairLiter" , pPairLiter , " null" , False )
@@ -13,27 +13,53 @@ testsPairLiter
     , ( "pPairLiter" , pPairLiter , " null" , False )
     , ( "pPairLiter" , pPairLiter , "Null " , False ) ]
 
-testArrayLiter 
+tArrayLiter 
   = [ ( "pArrayLiter" , pArrayLiter , "" ,   True  )
     , ( "pArrayLiter" , pArrayLiter , " " ,  True  )
     , ( "pArrayLiter" , pArrayLiter , "(" ,  True  ) ]
 
+tIntSign 
+  = [ ( "pIntSign" , pIntSign , "+" ,   True  ) 
+    , ( "pIntSign" , pIntSign , "-" ,   True  ) 
+    , ( "pIntSign" , pIntSign , "a" ,   True  ) 
+    , ( "pIntSign" , pIntSign , "a" ,   True  ) 
+    , ( "pIntSign" , pIntSign , "" ,    True  ) ]
+
+tIntLiter
+  = [ ( "pIntLiter" , pIntLiter , "12345" , True ) 
+    , ( "pIntLiter" , pIntLiter , "+12345" , True )
+    , ( "pIntLiter" , pIntLiter , "-12345" , True )
+    , ( "pIntLiter" , pIntLiter , "0" , True )
+    , ( "pIntLiter" , pIntLiter , "+0" , True )
+    , ( "pIntLiter" , pIntLiter , "-0" , True )
+    , ( "pIntLiter" , pIntLiter , "000000" , True )
+    , ( "pIntLiter" , pIntLiter , "-000111" , True )
+    , ( "pIntLiter" , pIntLiter , "+111100" , True )
+    , ( "pIntLiter" , pIntLiter , "" , False )
+    , ( "pIntLiter" , pIntLiter , "+" , False )
+    , ( "pIntLiter" , pIntLiter , "-" , False )
+    , ( "pIntLiter" , pIntLiter , "a" , False )
+    , ( "pIntLiter" , pIntLiter , "+a" , False )
+    , ( "pIntLiter" , pIntLiter , "-a" , False )
+    , ( "pIntLiter" , pIntLiter , "hello" , False )
+    , ( "pIntLiter" , pIntLiter , "+1x" , True ) ]
+
+
+runAll = do
+  runTests tPairLiter
+  runTests tArrayLiter
+  runTests tIntSign
+  runTests tIntLiter
+  runTests tArrayElem
+
 
 runTest ( fname , parser , input , pass ) = case regularParse parser input of 
-  Left  e -> reportTest $ if not pass then 
-                            "OK  Failed as expected!"
-                          else 
-                            "='( Failed but was supposed to pass... ( " 
-                            ++ show e ++ ")"
-
-  Right r -> reportTest $ if pass then 
-                            "OK  Success as expected!"
-                          else 
-                            "='( Success but was suppoed to fail... (" 
-                            ++ show r ++ ")"
-  where
-    reportTest msg = putStrLn $ fname ++ ":\t\"" ++ input ++ "\"\t ... " ++ msg ++ "\n"
-
+  Left  e -> log $ if not pass then "OK it failed!"
+                   else "='( Failed but should've passed... ("  ++ show e ++ ")"
+  Right r -> log $ if pass then "OK it succeeded! (" ++ show r ++ ")" 
+                   else "='( Success but shoudl've failed... (" ++ show r ++ ")"
+  where 
+    log msg = putStrLn $ fname ++ ":\t\"" ++ input ++ "\"\t ... " ++ msg ++ "\n"
 
 
 
