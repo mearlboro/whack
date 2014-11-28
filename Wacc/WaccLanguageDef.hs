4ba09a9cf9cf8967879b908b3cfa2067aa06398e
Madalina-Ioana Sas <ms6413@vm-shell1.doc.ic.ac.uk>
2014-11-28 17:49:20 +0000

    Added operator precedence in LanguageDef, types and cleanups.

--- Wacc/WaccLanguageDef.hs
+++ Wacc/WaccLanguageDef.hs
@@ -2,42 +2,44 @@
 -- :: 2. WACC Language Definition ::::::::::::::::::::::::::::::::::::::::::: --
 -- :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: --
 
-module Wacc.WaccLanguageDef 
-( waccIdentifier 
-, waccReserved   
-, waccReservedOp 
-, waccCharLiter  
-, waccStrLiter   
-, waccInteger   
-, waccNatural   
-, waccWhiteSpace 
-, waccParens     
-, waccBrackets   
-, waccSemicolon      
-, waccComma      
-, waccColon      
-, waccSemiSep    
-, waccSemiSep1   
-, waccCommaSep   
-, waccCommaSep1  
+module Wacc.WaccLanguageDef
+( waccIdentifier
+, waccReserved
+, waccReservedOp
+, waccCharLiter
+, waccStrLiter
+, waccInteger
+, waccNatural
+, waccWhiteSpace
+, waccParens
+, waccBrackets
+, waccSemicolon
+, waccComma
+, waccColon
+, waccSemiSep
+, waccSemiSep1
+, waccCommaSep
+, waccCommaSep1
 , waccOperators
 , waccLexeme
 ) where
 
-import Wacc.WaccDataTypes 
+import Wacc.WaccDataTypes
 
+import Control.Monad.Identity
 import Text.ParserCombinators.Parsec
 import Text.ParserCombinators.Parsec.Expr
 import Text.ParserCombinators.Parsec.Language ( emptyDef , haskellStyle )
 import qualified Text.ParserCombinators.Parsec.Token as Token
 
-
+--------------------------------------------------------------------------------
 reservedWords :: [ String ]
-reservedWords 
+reservedWords
   = [  "begin",     "end",   "is", "skip", "read", "free", "return",  "exit"
     ,  "print", "println",   "if", "then", "else",   "fi",  "while",    "do"
     ,   "done", "newpair", "call",  "fst",  "snd",  "int",   "bool",  "char"
     , "string",    "pair",  "len",  "ord",  "chr", "null",   "true", "false" ]
+
 
 reservedOps :: [ String ]
 reservedOps
@@ -45,7 +47,8 @@ reservedOps
     , "!=", "<",   ">",  "<=",  ">=", "&&"
     , "||", "%", "len", "ord", "chr",  "!" ]
 
--- TODO write type signature
+
+languageDef :: Token.LanguageDef sf
 languageDef
   = emptyDef
   { Token.commentLine     = "#"
@@ -54,58 +57,68 @@ languageDef
   , Token.identLetter     = alphaNum <|> char '_'
   , Token.reservedNames   = reservedWords
   , Token.reservedOpNames = reservedOps
-  --, Token.opStart         = Token.opStart  haskellStyle
-  --, Token.opLetter        = Token.opLetter haskellStyle
   , Token.caseSensitive   = True }
 
--- TODO write type signature
+
+lexer :: Token.GenTokenParser String st Identity
 lexer = Token.makeTokenParser languageDef
 
 
--- TODO write type signatures
-waccIdentifier = Token.identifier    lexer 
-waccReserved   = Token.reserved      lexer 
-waccReservedOp = Token.reservedOp    lexer 
-waccCharLiter  = Token.charLiteral   lexer 
+waccIdentifier = Token.identifier    lexer
+waccReserved   = Token.reserved      lexer
+waccReservedOp = Token.reservedOp    lexer
+waccCharLiter  = Token.charLiteral   lexer
 waccStrLiter   = Token.stringLiteral lexer
-waccInteger    = Token.integer       lexer 
-waccNatural    = Token.natural       lexer 
-waccWhiteSpace = Token.whiteSpace    lexer 
+waccInteger    = Token.integer       lexer
+waccNatural    = Token.natural       lexer
+waccWhiteSpace = Token.whiteSpace    lexer
 waccParens     = Token.parens        lexer
 waccBrackets   = Token.brackets      lexer
-waccSemicolon  = Token.semi          lexer 
-waccComma      = Token.comma         lexer 
-waccColon      = Token.colon         lexer 
-waccSemiSep    = Token.semiSep       lexer 
-waccSemiSep1   = Token.semiSep1      lexer 
-waccCommaSep   = Token.commaSep      lexer 
-waccCommaSep1  = Token.commaSep1     lexer 
-waccLexeme     = Token.lexeme        lexer 
+waccSemicolon  = Token.semi          lexer
+waccComma      = Token.comma         lexer
+waccColon      = Token.colon         lexer
+waccSemiSep    = Token.semiSep       lexer
+waccSemiSep1   = Token.semiSep1      lexer
+waccCommaSep   = Token.commaSep      lexer
+waccCommaSep1  = Token.commaSep1     lexer
+waccLexeme     = Token.lexeme        lexer
 
--- TODO write type signature
+
+waccOperators :: [[Operator Char st Expr]]
 waccOperators
-  = [ [ Prefix ( waccOperators' "!"    $ UnaryOperExpr  NotUnOp  )           ]
-    , [ Prefix ( waccOperators' "len " $ UnaryOperExpr  LenUnOp  )           ]
-    , [ Prefix ( waccOperators' "ord " $ UnaryOperExpr  OrdUnOp  )           ]
-    , [ Prefix ( waccOperators' "chr " $ UnaryOperExpr  ChrUnOp  )           ]
-    , [ Prefix ( waccOperators' "-"    $ UnaryOperExpr  NegUnOp  )           ]
-    , [ Infix  ( waccOperators' "+"    $ BinaryOperExpr AddBinOp ) AssocLeft ]
-    , [ Infix  ( waccOperators' "-"    $ BinaryOperExpr SubBinOp ) AssocLeft ]
-    , [ Infix  ( waccOperators' "*"    $ BinaryOperExpr MulBinOp ) AssocLeft ]
-    , [ Infix  ( waccOperators' "/"    $ BinaryOperExpr DivBinOp ) AssocLeft ]
-    , [ Infix  ( waccOperators' "%"    $ BinaryOperExpr ModBinOp ) AssocLeft ]
-    , [ Infix  ( waccOperators' "<"    $ BinaryOperExpr LsBinOp  ) AssocLeft ]
-    , [ Infix  ( waccOperators' ">"    $ BinaryOperExpr GtBinOp  ) AssocLeft ]
-    , [ Infix  ( waccOperators' "<="   $ BinaryOperExpr LEBinOp  ) AssocLeft ]
-    , [ Infix  ( waccOperators' ">="   $ BinaryOperExpr GEBinOp  ) AssocLeft ]
-    , [ Infix  ( waccOperators' "=="   $ BinaryOperExpr EqBinOp  ) AssocLeft ]
-    , [ Infix  ( waccOperators' "!="   $ BinaryOperExpr NEBinOp  ) AssocLeft ]
-    , [ Infix  ( waccOperators' "&&"   $ BinaryOperExpr AndBinOp ) AssocLeft ]
-    , [ Infix  ( waccOperators' "||"   $ BinaryOperExpr OrrBinOp ) AssocLeft ] ]
+  -- |Unary operators have the same precedence, which is 0.
+ = [ [ Prefix ( waccOperators' "!"   $ UnaryOperExpr  NotUnOp  )
+   ,   Prefix ( waccOperators' "len" $ UnaryOperExpr  LenUnOp  )
+   ,   Prefix ( waccOperators' "ord" $ UnaryOperExpr  OrdUnOp  )
+   ,   Prefix ( waccOperators' "chr" $ UnaryOperExpr  ChrUnOp  )
+   ,   Prefix ( waccOperators' "-"   $ UnaryOperExpr  NegUnOp  )           ]
+
+  -- |Arithmetic operators *, /, % have precedence 1.
+   , [ Infix  ( waccOperators' "*"   $ BinaryOperExpr MulBinOp ) AssocLeft
+   ,   Infix  ( waccOperators' "/"   $ BinaryOperExpr DivBinOp ) AssocLeft
+   ,   Infix  ( waccOperators' "%"   $ BinaryOperExpr ModBinOp ) AssocLeft ]
+
+  -- |Arithmetic operators +, - have precedence 2.
+   , [ Infix  ( waccOperators' "+"   $ BinaryOperExpr AddBinOp ) AssocLeft
+   ,   Infix  ( waccOperators' "-"   $ BinaryOperExpr SubBinOp ) AssocLeft ]
+
+  -- |Relational operators have precedence 3.
+   , [ Infix  ( waccOperators' "<"   $ BinaryOperExpr LsBinOp  ) AssocLeft
+   ,   Infix  ( waccOperators' ">"   $ BinaryOperExpr GtBinOp  ) AssocLeft
+   ,   Infix  ( waccOperators' "<="  $ BinaryOperExpr LEBinOp  ) AssocLeft
+   ,   Infix  ( waccOperators' ">="  $ BinaryOperExpr GEBinOp  ) AssocLeft ]
+
+  -- |Equality and inequality operators have precedence 4.
+   , [ Infix  ( waccOperators' "=="  $ BinaryOperExpr EqBinOp  ) AssocLeft
+   ,   Infix  ( waccOperators' "!="  $ BinaryOperExpr NEBinOp  ) AssocLeft ]
+
+  -- |Logical and operator has precedence 5.
+   , [ Infix  ( waccOperators' "&&"  $ BinaryOperExpr AndBinOp ) AssocLeft ]
+
+  -- |Logical or operator has precedence 6.
+   , [ Infix  ( waccOperators' "||"  $ BinaryOperExpr OrrBinOp ) AssocLeft ] ]
 
     where
 
         waccOperators' op expr = waccReservedOp op >> return ( expr )
 
-
-  
--- WaccTesting/WaccExamplesTester.hs
+++ WaccTesting/WaccExamplesTester.hs
@@ -17,7 +17,7 @@ import Control.Monad.IO.Class ( liftIO )
 import Control.Monad
 import Data.List 
 import System.Directory
-import System.FilePath        ( (</>) )
+import System.FilePath ( (</>) )
 
 --------------------------------------------------------------------------------
 
@@ -33,17 +33,17 @@ isHidden  =  not . isPrefixOf "."
 getRecursiveContents :: FilePath -> IO [ File ] 
 getRecursiveContents dir = do    
     children <- filter isHidden <$> getDirectoryContents dir
-    let extract name = do
-        let path = dir </> name 
-        isDirectory <- doesDirectoryExist path
-        if isDirectory 
-           then getRecursiveContents path 
-           else return [( name , path )]
+    let extract name = do {
+      let path = dir </> name 
+    ; isDirectory <- doesDirectoryExist path
+    ; if   isDirectory 
+      then getRecursiveContents path 
+      else return [( name , path )]
+    }
     concat <$> forM children extract
 
 --------------------------------------------------------------------------------
 
--- TODO: add comment/description
 testOne :: FilePath -> IO ()
 testOne path = do 
   -- Read source file 
@@ -66,69 +66,65 @@ parseOne :: Bool
          -> File    -- * Is it supposed to pass (True) or fail (False) ?
          -> IO Bool
 parseOne shouldPass ( name , path ) = do 
-  -- Read source file 
-  source <- readFile path -- putStrLn $ source
-  
-  -- Parse source file
-  let result = parseWithEof pProgram source
-  
-  -- Error message expected by LabTS
-  let errorMessage = "" -- "#syntax_error#\nexit:\n100\n"
-
-  -- Invalid program yet the parser parsed something... but what? 
-  let handlePhantomParse r = do 
-      putStrLn $ "FAILED (" ++ path ++ ")\n" ++ show r
-      -- "Parser was supposed to fail but it parsed this: " ++ show r 
-  
-
-  -- Parser failed to parse something it was supposed to be able to parse
-  let handleFail e = do 
-      putStrLn $ replicate 80 '~' ++ "\nFAILED (" ++ path ++ ")\n" ++ show e
-
-  -- Get the result and act accordingly
-  case result of -- putStrLn $ show result
-      Right r -> if   shouldPass 
-                 then do 
-
-                  let errs = unlines ( checkProgram r )
-                  when ( length errs > 0 ) ( do  
-                      putStrLn ( replicate 80 '*' ++ "\n*..." ++ 
-                                 drop ( length path - 74 ) path ++ " *" )
-                      putStrLn $ replicate 80 '*'
+    -- Read source file 
+    source <- readFile path -- putStrLn $ source
     
-                      putStrLn $ show r ++ "\n"
-                      putStrLn $ concat ( replicate 10 "~@" )
-                      putStrLn ("Semantic Errors: " )
-                      putStrLn ( errs ) )
+    -- Parse source file
+    let result = parseWithEof pProgram source
+    
+    -- Invalid program yet the parser parsed something... 
+    let handlePhantomParse r = do 
+        putStrLn $ replicate 80 '~' ++ "\nFAILED (" ++ path ++ ")\n" ++ show r 
+    
+    -- Parser failed to parse something it was supposed to be able to parse
+    let handleFail e = do 
+        putStrLn $ replicate 80 '~' ++ "\nFAILED (" ++ path ++ ")\n" ++ show e
 
-                  --putStrLn ( show $ augmentProgram r ) >> 
-                  --putStrLn ( replicate 80 '#') >> 
-                  return True 
-                 else handlePhantomParse r >> return False
+    -- Get the result and act accordingly 
+    case result of 
+        -- Performs semantic check
+        Right r -> if shouldPass 
+            then do 
+                let errs = unlines ( checkProgram r )
+                -- If there are semantic errors
+                when ( length errs > 0 ) ( do  
+                    putStrLn ( replicate 80 '*' ++ "\n*..." ++ 
+                               drop ( length path - 74 ) path ++ " *" )
+                    putStrLn $ replicate 80 '*'
+            
+                    putStrLn $ show r ++ "\n"
+                    putStrLn $ concat ( replicate 10 "~@" )
+                    putStrLn ("Semantic Errors: " )
+                    putStrLn ( errs ) )
+                if length errs > 0
+                    then return False
+                    else return True
+            else handlePhantomParse r >> return False
 
-      Left  e -> if   shouldPass 
-                 then handleFail e >> return False 
-                 else putStr errorMessage >> return True
+        Left e -> if shouldPass 
+            -- then handleFail e >> return False
+            then return False 
+            else putStr "Exit: 100\n" >> return True
 
 --------------------------------------------------------------------------------
 
-parseBunch shouldPass dir = do 
-  -- Is the file at file path a Wacc file?
-  let isWacc = isSuffixOf ".wacc"
-
-  -- All valid or invalid wacc programs
-  programs <- filter ( isWacc . snd ) <$> getRecursiveContents dir 
-
-  -- Parse all and find out how many passed or failed when they should have
-  behavedWell <- length . filter id <$> forM programs ( parseOne shouldPass )
-
-  -- The total number of programs parsed
-  let total = length programs
-
-  -- Print outcome
-  putStrLn $ replicate 80 '*'
-  putStr   $ show behavedWell ++ "/" ++ show total ++ " :: " 
-  putStrLn $ show ( total - behavedWell ) ++ " behaved badly!"  
+parseGroup shouldPass dir = do 
+    -- Is the file at file path a Wacc file?
+    let isWacc = isSuffixOf ".wacc"
+  
+    -- All valid or invalid wacc programs
+    programs <- filter ( isWacc . snd ) <$> getRecursiveContents dir 
+  
+    -- Parse all and find out how many passed or failed when they should have
+    behavedWell <- length . filter id <$> forM programs ( parseOne shouldPass )
+  
+    -- The total number of programs parsed
+    let total = length programs
+  
+    -- Print outcome
+    putStrLn $ replicate 80 '*'
+    putStr   $ show behavedWell ++ "/" ++ show total ++ " :: " 
+    putStrLn $ show ( total - behavedWell ) ++ " behaved badly!"  
 
 --------------------------------------------------------------------------------
 
@@ -136,10 +132,8 @@ main = do
   -- ../WaccCompiler.hs_directory/wacc_examples
   pwd <- flip (++) "/wacc_examples/" <$> getCurrentDirectory 
   -- Check valid programs
-  parseBunch True ( pwd ++ "valid" ) 
-  -- Check invalid programs
-  parseBunch False ( pwd ++ "syntaxErr" )
-  -- TODO: reimplement to be False 
-  parseBunch True  ( pwd ++ "semanticErr" ) 
+  parseGroup True  ( pwd ++ "valid"       ) 
 
- 
+  -- Check invalid programs
+  parseGroup False ( pwd ++ "syntaxErr"   ) 
+  parseGroup False ( pwd ++ "semanticErr" ) 
